6ae590f1e13133547a699273296c827c3efc00ab
Eduardo Augusto Finger Ballico <ballico591@gmail.com>
2021-11-29 17:15:08 -0300

    Pesquisas

--- src/Lixo/acesso/Acesso.java
+++ src/Lixo/acesso/Acesso.java
@@ -8,9 +8,16 @@ import java.util.Date;
 public class Acesso {
 
 	private Date dataAcesso;
-
 	private Cliente cliente;
-
 	private Entretenimento entretenimento;
 
+	public Acesso(Date dataAcesso, Cliente cliente, Entretenimento entretenimento){
+		this.dataAcesso = dataAcesso;
+		this.cliente = cliente;
+		this.entretenimento = entretenimento;
+	}
+
+	public Date getDataAcesso() { return dataAcesso; }
+	public Cliente getCliente() { return cliente; }
+	public Entretenimento getEntretenimento() { return entretenimento; }
 }
--- src/Lixo/acesso/ListaAcessos.java
+++ src/Lixo/acesso/ListaAcessos.java
@@ -3,12 +3,29 @@ package Lixo.acesso;
 import Lixo.cliente.Cliente;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ListaAcessos {
 
     private ArrayList<Acesso> acessos;
+    private ArrayList<Acesso> consAtual;
 
-    public void cadastra(Acesso a){
+    public void cadastra(Acesso a) {
         acessos.add(a);
+    }
+
+    public ArrayList<Acesso> pesquisaAcesso(Date data) { //throws exception
+
+        if (this.acessos.isEmpty()) { return null; }//throw new exception(); }
+
+        this.consAtual = new ArrayList<>();
+
+        for (Acesso ac : this.acessos) {
+            if (data.equals(ac.getDataAcesso())) {
+                consAtual.add(ac);
+            }
+        }
+        if (consAtual.isEmpty()) { return null; } //throw new exception(); }
+        return consAtual;
     }
 }
